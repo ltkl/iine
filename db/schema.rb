e2d217cb1853c762d4dbce9d1f72b6fa0e3c4fa5
Tatsuya Kawashima <tatsuya.liamg@gmail.com>
2016-01-23 22:56:52 +0900

    scaffold User

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160123132830) do
+ActiveRecord::Schema.define(version: 20160123134012) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
